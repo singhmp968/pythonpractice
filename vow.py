3736ff27e195d63581e2d7020fb544db0a76b1ac
singhmp968 <31666974+singhmp968@users.noreply.github.com>
2019-07-02 22:55:11 +0530

    Update vow.py

--- vow.py
+++ vow.py
@@ -1,7 +1,12 @@
 x=str(input())
-li=['a','e','i','o','u']
+li=['a','e','i','o','u','A','E','I','O','U']
+li2=['B','C','D','F','G','H','J','K','L','M','N','P','Q','R',
+'S','T','V','W','X','Y','Z','b','c','d','f',
+    'g', 'h', 'j', 'k','l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 if x in li:
     print("Vowel")
+elif x in li2:
+    print("Constant")
 else:
     print("invalid")
       
