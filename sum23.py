bced896a3c1b1d365a33fb2ed45a1e9b2a199583
singhmp968 <31666974+singhmp968@users.noreply.github.com>
2019-07-06 09:53:46 +0530

    Update sum23.py

--- sum23.py
+++ sum23.py
@@ -1,8 +1,7 @@
-
-lim=list(map(int,input("").split()))
-x1=len(li)
+li1=list(map(int,input("").split()))
+x1=len(li1)
 su1=0
 for i in range(0,x1,1):
-    su1=su1+li[i]
+    su1=su1+li1[i]
     
 print(su1)
