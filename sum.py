addad7932d4a2786f5871b01bbed325021b00e6b
singhmp968 <31666974+singhmp968@users.noreply.github.com>
2019-07-06 09:51:23 +0530

    Update and rename sum.py to sum23.py

--- sum.py
+++
@@ -1,8 +0,0 @@
-
-li=list(map(int,input("").split()))
-x=len(li)
-su=0
-for i in range(0,x,1):
-    su=su+li[i]
-    
-print(su)
---
+++ sum23.py
@@ -0,0 +1,8 @@
+
+lim=list(map(int,input("").split()))
+x1=len(li)
+su1=0
+for i in range(0,x1,1):
+    su1=su1+li[i]
+    
+print(su1)
