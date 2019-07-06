a879d270f575b52ef5dbe13c9b2606863942bebb
singhmp968 <31666974+singhmp968@users.noreply.github.com>
2019-07-06 11:22:21 +0530

    Create NumToWord.py

---
+++ NumToWord.py
@@ -0,0 +1,11 @@
+def fgh(l):
+    
+    agh={'1':'One',
+    '2':'Two','3':'Three','4':'Four','5':'Five','6':'Six',
+    '7':'Seven','8':'Eight','9':'Nine','10':'Ten'}
+    x=agh[l]
+    return x
+
+x=int(input())
+l=str(x)
+print(fgh(l))
