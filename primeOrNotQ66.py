3e127d521ac9ea0bdc0b70f86537617dd9557765
singhmp968 <31666974+singhmp968@users.noreply.github.com>
2019-07-08 12:32:47 +0530

    Create primeOrNotQ66.py

---
+++ primeOrNotQ66.py
@@ -0,0 +1,12 @@
+x=int(input())
+c=False
+if(x>1):
+    for i in range(2,x):
+        if(x%i==0):
+            print("no")
+            break
+    else:
+        print("yes")
+else:
+    print("no")
+
